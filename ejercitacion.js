981d19b40617a6b2cc8f599d74aeb09ebaee9c61
Mateo Gómez Fleytas <mateogf@hotmail.com>
2023-09-08 19:52:06 -0300

    pagina 81

--- ejercitacion.js
+++ ejercitacion.js
@@ -1,1 +1,7 @@
 
+console.log(-1/Infinity)
+let x = NaN
+console.log(x!=x) //TRUE: le asignamos a x nan. luego le decimos que x es distinto a x solo asi se puede
+console.log(-1/0)   //-Infinity: el resultado es tan ceracno a 0 que no llega al numero que detecta y devuelve...
+console.log(0/0)    //NaN: CUANDO una operacion aritmetica no es válida ya que no se puede dividir por 0
+console.log(1/0)
